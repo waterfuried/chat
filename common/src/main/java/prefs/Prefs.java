e2dad58c699a73597385264658d20394e2ca041f
waterfuried <cisternsystem@gmail.com>
2022-03-18 15:27:09 +0300

    переоформление чата: Maven-проект в отдельном репозитории

---
+++ common/src/main/java/prefs/Prefs.java
@@ -0,0 +1,36 @@
+package prefs;
+
+public class Prefs {
+    public static final int PORT = 8189; // порт подключения
+    public static final int TIMEOUT = 120; // время на прохождение авторизации, секунды
+
+    // признак команды
+    public static final String COM_ID = "/";
+
+    // команды сервера
+    public static final String COM_QUIT = "end";
+    public static final String COM_AUTHORIZE = "auth";
+    public static final String COM_REGISTER = "reg";
+    public static final String COM_PRIVATE_MSG = "w";
+    public static final String COM_CLIENT_LIST = "clientlist";
+    // новая команда
+    public static final String COM_CHANGE_NICK = "nick";
+
+    // ответы сервера на запросы
+    public static final String SRV_AUTH_OK = "auth_ok";
+    public static final String SRV_REG_ACCEPT = "reg_ok";
+    public static final String SRV_REG_FAULT = "reg_fault";
+    // ответы сервера на новую команду
+    public static final String SRV_CHANGE_OK = "change_ok";
+    public static final String SRV_CHANGE_FAULT = "change_fault";
+
+    public static String getCommand (String cmdName, String ... args) {
+        if (args == null || args.length == 0)
+            return COM_ID + cmdName;
+
+        StringBuilder sb = new StringBuilder(COM_ID + cmdName);
+        for (String s : args)
+            sb.append(" ").append(s);
+        return sb.toString();
+    }
+}
---
+++ server/src/main/java/authService/AuthServiceSimple.java
@@ -0,0 +1,13 @@
+package authService;
+
+import java.util.*;
+
+public class AuthServiceSimple extends AuthServiceCommon {
+    public AuthServiceSimple() {
+        List<UserData> users = new ArrayList<>();
+        users.add(new UserData("qwe", "qwe", "qwe"));
+        users.add(new UserData("asd", "asd", "asd"));
+        users.add(new UserData("zxc", "zxc", "zxc"));
+        super.setUsers(users);
+    }
+}
